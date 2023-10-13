7dfe10d98dd0a1f8196d718c5c8884624d40b4ea
jimenagr <a01252605@itesm.mx>
2023-10-13 01:51:38 -0600

    Initial Commit

---
+++ Reto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Reto
+//
+//  Created by Jimena Gallegos on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
